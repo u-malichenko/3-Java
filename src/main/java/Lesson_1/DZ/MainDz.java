440c225478977b8508068ff16c653921c1f928c9
u.malichenko <u.malichenko@gmail.com>
2020-01-29 16:00:50 +0300

    1 часть домашнего задания (копировать элемент массива)

---
+++ src/main/java/Lesson_1/DZ/MainDz.java
@@ -0,0 +1,74 @@
+package Lesson_1.DZ;
+
+import java.util.Arrays;
+
+/**
+ * 2. Написать метод, который преобразует массив в ArrayList;
+ */
+public class MainDz {
+    public static void main(String[] args) {
+        System.out.println("1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);");
+
+        Integer[] inums = {1, 2, 3, 4, 5}; //создали массив
+        MyObj<Integer> iob = new MyObj<>(inums); //создали обект из массива
+        iob.change(iob, 0); //заустили метод смены значений, второй параметр задает начальную позицию замены
+
+        Double[] dnums = {1.0, 2.0, 3.0, 4.0, 5.0};
+        MyObj<Double> dob = new MyObj<>(dnums);
+        dob.change(dob, 3);
+
+        String[] strings = {"a", "b", "c", "d", "e"};
+        MyObj<String> sob = new MyObj<>(strings);
+        sob.change(sob, 4);
+
+        MyObj[] myObjs = {iob, dob, sob};
+        MyObj<MyObj> oob = new MyObj<>(myObjs);
+        oob.change(oob, 1);
+
+        System.out.println("Конец 1го задания.");
+
+
+
+    }
+}
+
+class MyObj<T> { //описываем ограничения сверху - extends Number
+    private T[] array;
+
+    public MyObj(T[] mass) {
+        this.array = mass;
+    }
+    public T[] getArray() {
+        return array;
+    }
+
+    /**
+     * 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+     */
+    public void change(MyObj<T> myObj, Integer shift) { //ограничения снизу накладываем в методе - super Number
+        T[] array = myObj.getArray();
+        String string = Arrays.toString(array);
+        System.out.println(string + ", исходный массив");
+        if (myObj.array.length < (shift + 1) || shift <= 0) { //если длинна массива меньше сдвига+1 и отрицательных инедексов
+            System.out.println("Массив не изменен");
+        } else {
+            T temp = myObj.array[shift - 1];
+            myObj.array[shift - 1] = myObj.array[shift];
+            myObj.array[shift] = temp;
+            array = myObj.getArray();
+            string = Arrays.toString(array);
+            System.out.println(string + " заменены элементы " + shift +" и "+ (shift+1));
+        }
+    }
+
+    public double avg() {
+        double sum = 0.0;
+
+//        for (int i = 0; i < nums.length; i++) {
+//            sum += nums[i].doubleValue();
+//        }
+
+        return sum / array.length;
+    }
+
+}
