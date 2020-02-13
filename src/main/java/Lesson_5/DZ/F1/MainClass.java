2a088950bfc43b92105c622c16faac846c09886e
u.malichenko <u.malichenko@gmail.com>
2020-02-13 14:24:02 +0300

    test

--- src/main/java/Lesson_5/DZ/F1/MainClass.java
+++ src/main/java/Lesson_5/DZ/F1/MainClass.java
@@ -2,17 +2,39 @@ package Lesson_5.DZ.F1;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
+
+    public static int getCarsCount() {
+        return CARS_COUNT;
+    }
+
     public static void main(String[] args) {
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40)); //набор препятствий
+        Car[] cars = new Car[CARS_COUNT]; //набор машин
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        try {
+
+            Thread  t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < cars.length; i++) {
+                        cars[i] = new Car(race, 20 + (int) (Math.random() * 10)); //создание машины  и установка рандомной скорости20+-10
+                    }
+                }
+            });
+            t1.start();
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
-        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        for (int i = 0; i < cars.length; i++) {
+            try {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/Lesson_5/DZ/F1/Race.java
+++ src/main/java/Lesson_5/DZ/F1/Race.java
@@ -9,4 +9,5 @@ public class Race {
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
+
 }
