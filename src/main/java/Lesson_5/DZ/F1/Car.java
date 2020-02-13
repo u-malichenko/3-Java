1afe6ab69aa944d7828617f5c1218eceeae1e76f
u.malichenko <u.malichenko@gmail.com>
2020-02-13 16:37:22 +0300

    ДЗ 5 не успеваю доделать дз с гонками(((

--- src/main/java/Lesson_5/DZ/F1/Car.java
+++ src/main/java/Lesson_5/DZ/F1/Car.java
@@ -1,12 +1,6 @@
 package Lesson_5.DZ.F1;
 
-import java.util.concurrent.*;
-
 public class Car implements Runnable {
-
-    int carsCount = MainClass.getCarsCount();
-    CyclicBarrier cb = new CyclicBarrier(carsCount);
-    ExecutorService pool = Executors.newFixedThreadPool(carsCount);
 
     private static int CARS_COUNT;
     static {
@@ -21,46 +15,29 @@ public class Car implements Runnable {
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
-        pool.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println(name + " готовится");
-                    Thread.sleep(500 + (int)(Math.random() * 800));
-                    System.out.println(name + " готов");
-                    cb.await();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
+
     @Override
     public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            MainClass.BARRIER.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void F1 (){
         for (int i = 0; i < race.getStages().size(); i++) {
-            /**
-             *                  * тут надо вставить Семафор
-             *                  * Semaphore smp = new Semaphore(2); половина!!!
-             */
             race.getStages().get(i).go(this);
         }
-        pool.shutdown();
     }
-//    @Override
-//    public void run() {
-//        for (int i = 0; i < race.getStages().size(); i++) {
-//            /**
-//             *                  * тут надо вставить Семафор
-//             *                  * Semaphore smp = new Semaphore(2); половина!!!
-//             */
-//            race.getStages().get(i).go(this);
-//        }
-//        pool.shutdown();
-//    }
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
+
 }
--- src/main/java/Lesson_5/DZ/F1/MainClass.java
+++ src/main/java/Lesson_5/DZ/F1/MainClass.java
@@ -1,5 +1,10 @@
 package Lesson_5.DZ.F1;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
@@ -7,34 +12,77 @@ public class MainClass {
         return CARS_COUNT;
     }
 
+    private static final ExecutorService POOL = Executors.newFixedThreadPool(CARS_COUNT);
+    public static CyclicBarrier BARRIER = new CyclicBarrier(CARS_COUNT+1);
+
     public static void main(String[] args) {
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40)); //набор препятствий
         Car[] cars = new Car[CARS_COUNT]; //набор машин
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        try {
 
-            Thread  t1 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < cars.length; i++) {
-                        cars[i] = new Car(race, 20 + (int) (Math.random() * 10)); //создание машины  и установка рандомной скорости20+-10
-                    }
-                }
-            });
-            t1.start();
-            t1.join();
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10)); //создание машины  и установка рандомной скорости20+-10
+            POOL.execute( cars[i]);
+        }
+        try {
+            MainClass.BARRIER.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
-
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
         for (int i = 0; i < cars.length; i++) {
-            try {
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
+        POOL.shutdown();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
+
+
+// class Ferry {
+//    private static final CyclicBarrier BARRIER = new CyclicBarrier(3, new FerryBoat());
+//    //Инициализируем барьер на три потока и таском, который будет выполняться, когда
+//    //у барьера соберется три потока. После этого, они будут освобождены.
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        for (int i = 0; i < 9; i++) {
+//            new Thread(new Car(i)).start();
+//            Thread.sleep(400);
+//        }
+//    }
+//
+//    //Таск, который будет выполняться при достижении сторонами барьера
+//    public static class FerryBoat implements Runnable {
+//        @Override
+//        public void run() {
+//            try {
+//                Thread.sleep(500);
+//                System.out.println("Паром переправил автомобили!");
+//            } catch (InterruptedException e) {
+//            }
+//        }
+//    }
+//
+//    //Стороны, которые будут достигать барьера
+//    public static class Car implements Runnable {
+//        private int carNumber;
+//
+//        public Car(int carNumber) {
+//            this.carNumber = carNumber;
+//        }
+//
+//        @Override
+//        public void run() {
+//            try {
+//                System.out.printf("Автомобиль №%d подъехал к паромной переправе.\n", carNumber);
+//                //Для указания потоку о том что он достиг барьера, нужно вызвать метод await()
+//                //После этого данный поток блокируется, и ждет пока остальные стороны достигнут барьера
+//                BARRIER.await();
+//                System.out.printf("Автомобиль №%d продолжил движение.\n", carNumber);
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
+//}
